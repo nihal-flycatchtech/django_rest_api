138365aab35822ace88f79362ed6888b6c5e559f
@nihal987 <nihal.basheer@calibroz.com>
2021-09-09 13:32:27 +0530

    fix: registrationview, url

--- create_user/api/urls.py
+++ create_user/api/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from create_user.api.views import Registration_view,HelloView
+from create_user.api.views import RegistrationView, HelloView
 
-app_name='create_user'
+app_name = 'create_user'
 
-urlpatterns=[
-    path('register',Registration_view.as_view(),name="register"),
-    path('view',HelloView.as_view(),name="view")
+urlpatterns = [
+    path('register', RegistrationView.as_view(), name="register"),
+    path('view', HelloView.as_view(), name="view")
 ]
