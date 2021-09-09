3d2ec66b5824241700db0c2606b8845024bdf4bc
Anvar Nazar <anvarnasar@ymail.com>
2021-09-09 13:04:21 +0530

    refactor: format views.py

--- create_user/api/views.py
+++ create_user/api/views.py
@@ -7,28 +7,25 @@ from rest_framework.views import APIView
 from create_user.api.serializers import Registerserializers
 
 
-class Registration_view(APIView):
-
-
-    def post(self,request):
-
-
-        if request.method=='POST':
-            serializer=Registerserializers(data=request.data)
-            data={}
+class RegistrationView(APIView):
+    def post(self, request):
+        if request.method == 'POST':
+            serializer = Registerserializers(data=request.data)
+            data = {}
             if serializer.is_valid():
                 account=serializer.save()
-                data['response']="successfully registered a new user"
-                data['email']=account.email
+                data['response'] = "successfully registered a new user"
+                data['email'] = account.email
                 data['username'] = account.username
             else:
-                data=serializer.errors
+                data = serializer.errors
             return Response(data)
+
 
 class HelloView(APIView):
     authentication_classes = [JWTAuthentication]
     permission_classes = (IsAuthenticated,)
-    def get(self,request):
+    def get(self, request):
         content = {'message': 'Hello,World'}
         return Response(content)
 
